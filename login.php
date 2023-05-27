c4af67c2bca84d015e963b3887ef6ef0a14a6b75
Yordi-Dereje <yordadereje@gmail.com>
2023-05-27 09:35:56 +0300

    temporary home page created

--- login.php
+++ login.php
@@ -16,7 +16,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 			$user_data = mysqli_fetch_assoc($result);
 			if($user_data['Password'] === $password){
 				$_SESSION['id'] = $user_data['id'];
-				header("Location: home.html");
+				header("Location: home.php");
 				die;
 			}
 			else{
--- temphome.php
+++ temphome.php
@@ -13,20 +13,18 @@ $user_data = check_login($con);
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="home.css?version=1" />
-  <link rel="stylesheet" href="darkmode.css">
-  <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet">
-  <title>Document</title>
+  <link rel="stylesheet" href="temphome.css" />
+  <title>Home page</title>
 </head>
-
-    <div class="home_whole">
-    <div class="home_sidebar">
+<body>
+  <div class="whole">
+    <div class="sidebar">
       <div class="profile">
         <h2><?php echo $user_data['UserName']; ?></h2>
         <h4><?php echo $user_data['FirstName'].' '.$user_data['LastName']; ?></h4>
         <ul>
-          <li><a href="#">Home</span></a></li>
-          <li><a href="#">Today</span></a></li>
+          <li><a href="temphome.php">Today</span></a></li>
+          <li><a href="newtask.php">New task</span></a></li>
           <li><a href="#">Upcoming</span></a></li>
           <li><a href="#">History</span></a></li>
           <li><a href="setting.html">Settings</span></a></li>
@@ -35,40 +33,31 @@ $user_data = check_login($con);
         </ul>
       </div>
     </div>
-    <div class="home_main">
-      <body class="light">
-      <input type="checkbox" id="toggle">
-      <label class="toggle" for="toggle">
-        <i class="bx bxs-sun"></i>
-        <i class="bx bx-moon"></i>
-        <span class="ball"></span>
-      </label>
 
-
-     <button> <a href="newtask.php?id=<?= $user_data['id']; ?>">Add new</a></button> 
-      
-<br><br>
     <div class="mainpart">
-        <?php
+      <p> This is the main display part </p>
+<!-- compare date to todays date and display only that and also arrange using priority -->
+      <?php
           $id = $user_data['id']; 
           $query2 = "select * from Tasks join Status_table on Tasks.Status=Status_table.Sid join Priority_table on Tasks.Priority=Priority_table.Pid  where UserID = '$id' ";
           $run_query = mysqli_query($con, $query2);
-          if(mysqli_num_rows($run_query) > 0){ ?>
-            <table class="tasktable" border="1" width=80%>
+          if(mysqli_num_rows($run_query) > 0){?>
+          <table class="tasktable" border="1" width=80%>
             <thead>
               <tr>
                 <th>Task</th>
                 <th>Description</th>
                 <th>Category</th>
                 <th>Date</th>
-                <th colspan=2>Time</th>
+                <th>Time</th>
                 <th>Status</th>
                 <th>Priority</th>
                 <th>Action</th>
               </tr>
             </thead>  
           <tbody>
-        <?php
+
+                    <?php
             foreach($run_query as $row){
               ?>
               <tr>
@@ -77,7 +66,6 @@ $user_data = check_login($con);
               <td><?= $row['Category']; ?></td>
               <td><?= $row['Date']; ?></td>
               <td><?= $row['Time_H']; ?></td>
-              <td><?= $row['Time_M']; ?></td>
               <td><?= $row['Sdes']; ?></td>
               <td><?= $row['Pdes']; ?></td>
               <td>
@@ -93,13 +81,9 @@ $user_data = check_login($con);
           }
           ?>
         </tbody>
-      
       </table>
-      
     </div>
 
-    </div>
   </div>
 </body>
 </html>
-<script src="darkmode.js"></script>
